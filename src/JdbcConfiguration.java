0150746a488cfe441a0476b5db06206c25639fb0
henriquewallace <henrique.wallace@hotmail.com>
2024-07-04 22:05:24 -0300

    feat: create jdbc postgresql connection

--- src/JdbcConfiguration.java
+++ src/JdbcConfiguration.java
@@ -1,2 +1,18 @@
+import java.sql.*;
+
 public class JdbcConfiguration {
+    public static void main(String[] args) {
+
+    }
+
+    private void postgreSqlConnection() {
+        try {
+            Connection connection = DriverManager
+                    .getConnection("jdbc:postgresql://localhost:5433/proposaldb", "postgres", "123");
+
+            Statement statement = connection.createStatement();
+        } catch (SQLException ex) {
+            ex.getMessage();
+        }
+    }
 }
