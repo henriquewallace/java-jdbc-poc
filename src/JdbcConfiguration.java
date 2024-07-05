12fb9c780a9990575f7b06fe7c5b1df444e51199
henriquewallace <henrique.wallace@hotmail.com>
2024-07-04 23:26:22 -0300

    feat: execute sql statement and return value

--- src/JdbcConfiguration.java
+++ src/JdbcConfiguration.java
@@ -2,17 +2,30 @@ import java.sql.*;
 
 public class JdbcConfiguration {
     public static void main(String[] args) {
-
-    }
-
-    private void postgreSqlConnection() {
         try {
-            Connection connection = DriverManager
-                    .getConnection("jdbc:postgresql://localhost:5433/proposaldb", "postgres", "123");
+            Statement statement = postgreSqlConnection();
+            String name = findUserNameById(statement);
 
-            Statement statement = connection.createStatement();
+            System.out.println(name);
+
         } catch (SQLException ex) {
             ex.getMessage();
         }
+    }
+
+    private static Statement postgreSqlConnection() throws SQLException {
+        Connection connection = DriverManager
+                    .getConnection("jdbc:postgresql://localhost:5433/proposaldb", "postgres", "123");
+
+            assert connection != null;
+            return connection.createStatement();
+    }
+
+    private static String findUserNameById(Statement statement) throws SQLException {
+        var sql = "select name from tb_user where id = 1";
+        ResultSet resultSet = statement.executeQuery(sql);
+        resultSet.next();
+
+        return resultSet.getString(1);
     }
 }
